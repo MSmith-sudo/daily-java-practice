78c4b9a35f20c10ccb3e77a64bf11584c6738052
MSmith-sudo <msmit640@charlotte.edu>
2025-08-21 17:11:41 +0000

    feat(arrays):Print all numbers in an array.

---
+++ Arrays/PrintAllNums.java
@@ -0,0 +1,9 @@
+public class PrintAllNums {
+    public static void main(String[] args) {
+        int[] nums = { 3, 7, 18, 25, 47, 52, 99 };
+
+        for (int i = 0; i < nums.length; i++) {
+            System.out.println(nums[i]);
+        }
+    }
+}
