ff0c8bd36368a9d928fd7dc859b0d3294b375229
MSmith-sudo <msmit640@charlotte.edu>
2025-09-11 00:24:21 +0000

    feat(extras): Factorial implemented iteratively (and recursive options)

---
+++ Extras/Factorial.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class Factorial{
+  public static void main(String[] args){
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Enter your number: ");
+    int n = sc.nextInt();
+    int result = 1;
+    for (int i = 1; i <= n; i++){
+      result *= i;
+    }
+    System.out.println("The factorial of " + n + " is " + result);
+    sc.close();
+  }
+}n
