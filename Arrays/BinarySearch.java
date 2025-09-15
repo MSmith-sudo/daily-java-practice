4c3a5a16f2714eb3e240904c49420314033b8d61
MSmith-sudo <msmit640@charlotte.edu>
2025-09-15 20:51:38 +0000

    feat(arrays): Binary search (iterative version).

---
+++ Arrays/BinarySearch.java
@@ -0,0 +1,24 @@
+public class BinarySearch{
+  private static int binarySearch(int[] arr, int target){
+    int left = 0;
+    int right = arr.length - 1;
+
+    while (left <= right){
+      int mid = left + (right - left) / 2;
+      if (arr[mid] == target) return mid;
+      if (arr[mid] < target) left = mid + 1;
+      else right = mid - 1;
+    }
+    return -1;
+  }
+
+  public static void main(String[] args){
+    int[] arr = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    int target1 = 5;
+    int target2 = 11;
+
+    System.out.println("The target is at index: " + binarySearch(arr, target1));
+    System.out.println("The target is at index: " + binarySearch(arr, target2));
+  
+  }
+}
