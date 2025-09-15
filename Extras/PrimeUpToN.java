424b613df32a1c67babf471c72f5a655b3164969
MSmith-sudo <msmit640@charlotte.edu>
2025-09-15 20:40:03 +0000

    feat(math): List all prime numbers up to n.

---
+++ Extras/PrimeUpToN.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+public class PrimeUpToN{
+ private static boolean isPrime(int n){
+   if (n <= 1) return false;
+   for (int i = 2; i <= Math.sqrt(n); i++){
+     if (n % i == 0) return false;
+   }
+   return true;
+ }
+
+  public static void main(String[] args){
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Enter a number: ");
+    int n = sc.nextInt();
+
+    System.out.println("Prime numbers up to " + n + ": ");
+    for (int i = 2; i <= n; i++){
+      if (isPrime(i)){
+        System.out.print(i + " ");
+      }
+    }
+    System.out.println();
+
+    sc.close();
+  }
+}
