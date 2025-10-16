7ca044ccb0e1feb375f38fb860e35142f8ae5a81
MSmith-sudo <msmit640@charlotte.edu>
2025-10-16 18:16:34 +0000

    feat(slidingwindow): Find length of longest substring without repeats.

---
+++ SlidingWindow/LongestSubstring.java
@@ -0,0 +1,23 @@
+import java.net.Authenticator.RequestorType;
+import java.util.*;
+
+public class LongestSubstring{
+  private static int longestSubstring(String s){
+    Set<Character> set = new HashSet<>();
+    int left = 0, maxLen = 0;
+    for (int right = 0; right < s.length(); right++){
+      while(set.contains(s.charAt(right))){
+        set.remove(s.charAt(left));
+        left++;
+      }
+      set.add(s.charAt(right));
+      maxLen = Math.max(maxLen, right - left + 1);
+    }
+    return maxLen;
+  }
+
+  public static void main(String[] args){
+    String s = "abcabcbb";
+    System.out.println(longestSubstring(s)); 
+  }
+}
